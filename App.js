ce9d64929aed95eb56dcd2d9380bc94677c48929
HeshanPravintha <31812933+HeshanPravintha@users.noreply.github.com>
2021-06-14 21:20:27 +0530

    Update App.js
    Flexbox Deep Dive

--- App.js
+++ App.js
@@ -3,15 +3,44 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput
-          placeholder="Course Goal"
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}
-        />
-        <Button title="Add" />
+    <View style={{
+      padding: 50,
+      flexDirection: 'row',
+      width: '80%',
+      height: 300,
+      justifyContent: 'space-around',
+      alignItems: 'stretch'
+    }}
+    >
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View />
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   );
 };
